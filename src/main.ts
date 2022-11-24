c0ea1af1e368d5d367138458ddcde4d3f62c1480
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-11-24 14:14:00 +0900

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import * as core from '@actions/core'
+import { P4 } from '@akiojin/p4-command'
+
+const IsWindows = process.platform.toLowerCase() === 'win32'
+
+async function Run(): Promise<void> 
+{
+    try {
+        if (!IsWindows) {
+            throw new Error('Not supported platform.')
+        }
+
+        P4.Initialize(core.getInput('ip'), core.getInput('username'), core.getInput('workspace'));
+
+        core.startGroup('p4 sync')
+        await P4.GetLatest(core.getInput('password'))
+        core.endGroup()
+    } catch (ex: any) {
+        core.setFailed(ex.message);
+    }
+}
+
+Run()
